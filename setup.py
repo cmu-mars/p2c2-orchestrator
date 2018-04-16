5e3201534b8176bcd2fa68909c3773e3e6415341
ChrisTimperley <christimperley@googlemail.com>
2018-04-16 14:55:12 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from glob import glob
+from setuptools import setup, find_packages
+
+setup(
+    name='orchestrator',
+    version='0.0.1',
+    description='Orchestrates the subsystems responsible for Phase II CP2',
+    author='Chris Timperley',
+    author_email='ctimperley@cmu.edu',
+    url='https://github.com/cmu-mars/p2c2-orchestrator',
+    license='mit',
+    python_requires='>=3.5',
+    install_requires=[
+        'bugzoo>=2.0.13',
+        'hulk',
+        'requests',
+        'flask'
+    ],
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    entry_points = {
+        'console_scripts': [
+            'orchestrator = orchestrator.server:main'
+        ]
+    }
+)
