b4488d3c63cd81ec6d0d769a1d5e274794268dc9
ChrisTimperley <christimperley@googlemail.com>
2018-04-26 22:47:11 -0400

    setup: use bugzoo >= 2.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     python_requires='>=3.5',
     install_requires=[
         'darjeeling>=0.0.18',
-        'bugzoo>=2.0.15',
+        'bugzoo>=2.1.0',
         # 'hulk',
         'requests',
         'flask'
