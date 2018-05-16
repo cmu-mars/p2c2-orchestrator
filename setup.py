7ca697b7c68a2f680e36d54eb2006df3c4ffd8a9
ChrisTimperley <christimperley@googlemail.com>
2018-05-15 21:23:54 -0400

    updated bugzoo version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     python_requires='>=3.5',
     install_requires=[
         'darjeeling>=0.1.2',
-        'bugzoo>=2.1.5',
+        'bugzoo>=2.1.6',
         'boggart>=0.1.1',
         'requests',
         'flask'
