aaa6d2737f593e2d42fcdc0e33bb78111ba08409
ChrisTimperley <christimperley@googlemail.com>
2018-05-16 20:28:48 -0400

    fixed multiline string

--- src/orchestrator/orchestrator.py
+++ src/orchestrator/orchestrator.py
@@ -311,8 +311,7 @@ class Orchestrator(object):
             Mutation("flip-boolean-operator", 1,
                      boggart.FileLocationRange.from_string("src/yujin_ocs/yocs_cmd_vel_mux/src/cmd_vel_mux_nodelet.cpp@40:6::42:77"),
                      {'1': '(cmd_vel_sub.allowed == VACANT)',
-                      '2': '(cmd_vel_sub.allowed == idx)    ||
-                            (cmd_vel_sub[idx].priority > cmd_vel_sub[cmd_vel_sub.allowed].priority)'})
+                      '2': '(cmd_vel_sub.allowed == idx) || (cmd_vel_sub[idx].priority > cmd_vel_sub[cmd_vel_sub.allowed].priority)'})  # noqa: pycodestyle
         ]
         # mutations = boggartd.mutations(self.baseline,
         #                                filepath=filename,
