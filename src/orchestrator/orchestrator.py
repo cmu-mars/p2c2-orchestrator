5fd9b485b057581efcf4b24ae42c501be89c4e94
ChrisTimperley <christimperley@googlemail.com>
2018-05-20 13:11:42 -0400

    disabled candidate limit

--- src/orchestrator/orchestrator.py
+++ src/orchestrator/orchestrator.py
@@ -437,7 +437,7 @@ class Orchestrator(object):
                     self.__searcher = Searcher(bugzoo=self.bugzoo,
                                                problem=problem,
                                                candidates=candidates,
-                                               num_candidates=attempts,
+                                               # num_candidates=attempts,
                                                time_limit=time_limit)
                     logger.debug("constructed search mechanism")
                     logger.info("beginning search")
