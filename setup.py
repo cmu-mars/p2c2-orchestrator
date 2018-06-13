7f9bab3cef247c862f6ae10949701a5b1c638d12
ChrisTimperley <christimperley@googlemail.com>
2018-06-13 11:15:29 -0400

    updated dependencies

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     license='mit',
     python_requires='>=3.5',
     install_requires=[
-        'darjeeling>=0.1.5',
-        'bugzoo>=2.1.8',
-        'boggart>=0.1.5',
+        'darjeeling>=0.1.6',
+        'bugzoo>=2.1.9',
+        'boggart>=0.1.6',
         'rooibos>=0.3.0',
         'requests',
         'flask'
