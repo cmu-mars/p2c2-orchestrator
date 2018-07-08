4734655766ec8bb91161bdf4bfc816d84a72d5c5
ChrisTimperley <christimperley@googlemail.com>
2018-07-08 16:23:41 -0400

    require darjeeling 0.1.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license='mit',
     python_requires='>=3.5',
     install_requires=[
-        'darjeeling>=0.1.9',
+        'darjeeling>=0.1.10',
         'bugzoo>=2.1.14',
         'boggart>=0.1.12',
         'rooibos>=0.3.0',
